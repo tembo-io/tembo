10697fb08eadeb49b960768b02e7878ba9e46866
Adam Hendel <ChuckHend@users.noreply.github.com>
2022-12-14 11:45:48 -0600

    remove comments

--- extensions/dev/http_webserver/benchmarking/src/db.rs
+++ extensions/dev/http_webserver/benchmarking/src/db.rs
@@ -8,8 +8,6 @@ use std::env;
 type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 pub type DbConnection = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
 
-// embed_migrations!();
-
 lazy_static! {
     static ref POOL: Pool = {
         let db_url = env::var("DATABASE_URL").expect("Database url not set");
@@ -21,7 +19,6 @@ lazy_static! {
 pub fn init() {
     lazy_static::initialize(&POOL);
     let conn = connection().expect("Failed to get db connection");
-    // embedded_migrations::run(&conn).unwrap();
 }
 
 pub fn connection() -> Result<DbConnection, CustomError> {
