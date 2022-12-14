67f76826d0a4cda4a8471752c16e13bad7427841
Adam Hendel <adam@coredb.io>
2022-12-14 11:34:40 -0600

    update env var names

--- extensions/dev/http_webserver/benchmarking/src/main.rs
+++ extensions/dev/http_webserver/benchmarking/src/main.rs
@@ -13,6 +13,7 @@ mod items;
 mod error_handler;
 mod schema;
 
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
@@ -24,8 +25,8 @@ async fn main() -> std::io::Result<()> {
     server = match listenfd.take_tcp_listener(0)? {
         Some(listener) => server.listen(listener)?,
         None => {
-            let host = env::var("HOST").expect("Please set host in .env");
-            let port = env::var("PORT").expect("Please set port in .env");
+            let host = env::var("WEBSERVER_HOST").expect("Set WEBSERVER_HOST in .env");
+            let port = env::var("WEBSERVER_PORT").expect("Set WEBSERVER_PORT in .env");
             server.bind(format!("{}:{}", host, port))?
         }
     };
