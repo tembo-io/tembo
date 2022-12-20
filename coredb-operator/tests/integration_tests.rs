e06561dad26a2c7906d8a7c89f4597ec9dbe60aa
Steven Miller <sjmiller609@gmail.com>
2022-12-20 10:36:51 -0500

    Hello world integration test

---
+++ coredb-operator/tests/integration_tests.rs
@@ -0,0 +1,13 @@
+
+// Include the #[ignore] macro on slow tests.
+// That way, 'cargo test' does not run them by default.
+// To run just these tests, use 'cargo test -- --ignored'
+// To run all tests, use 'cargo test -- --include-ignored'
+//
+// https://doc.rust-lang.org/book/ch11-02-running-tests.html
+
+#[test]
+#[ignore]
+fn it_is() {
+    assert!(true);
+}
