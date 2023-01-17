ee678e2189131885f00372738f6099e4496bd3c6
Steven Miller <sjmiller609@gmail.com>
2023-01-17 17:03:01 -0500

    Hello world, subcommand

--- coredb-cli/src/main.rs
+++ coredb-cli/src/main.rs
@@ -1,16 +1,32 @@
-#![allow(unused)]
+use clap::{Args, Parser, Subcommand};
 
-use clap::Parser;
-
-/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
 struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    path: std::path::PathBuf,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Adds files to myapp
+    Get(Get),
+}
+
+#[derive(Args)]
+struct Get {
+    name: String,
 }
 
 fn main() {
-    let args = Cli::parse();
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Get(name) => {
+            println!("'coredb get {:?}' was called", &name.name)
+        }
+    }
 }
