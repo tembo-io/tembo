63453147514960ab99c9083e543fa7bfa958b66c
Steven Miller <sjmiller609@gmail.com>
2023-01-19 16:26:51 -0500

    hello world CLI test

---
+++ coredb-cli/tests/integration_tests.rs
@@ -0,0 +1,16 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+const CARGO_BIN: &str = "coredb-cli";
+
+#[test]
+fn help() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin(CARGO_BIN)?;
+
+    cmd.arg("--help");
+    cmd.assert()
+        .stdout(predicate::str::contains("Usage: "));
+
+    Ok(())
+}
