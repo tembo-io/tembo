9ba9b12aa1f5fedf9847292f0b21ea49b87c497a
Steven Miller <sjmiller609@gmail.com>
2023-01-20 11:07:42 -0500

    [COR-146] Add tests for CLI (#55)

---
+++ coredb-cli/tests/integration_tests.rs
@@ -0,0 +1,44 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+const CARGO_BIN: &str = "coredb-cli";
+
+#[test]
+fn help() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin(CARGO_BIN)?;
+
+    cmd.arg("--help");
+    cmd.assert().stdout(predicate::str::contains("Usage: "));
+
+    Ok(())
+}
+
+#[test]
+fn create_dry_run() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin(CARGO_BIN)?;
+    cmd.arg("create")
+        .arg("db")
+        .arg("--dry-run")
+        .arg("sample-db");
+    cmd.assert()
+        .stdout(predicate::str::contains("kind: CoreDB"));
+
+    let mut cmd = Command::cargo_bin(CARGO_BIN)?;
+    cmd.arg("create")
+        .arg("--dry-run")
+        .arg("db")
+        .arg("sample-db");
+    cmd.assert()
+        .stdout(predicate::str::contains("kind: CoreDB"));
+
+    let mut cmd = Command::cargo_bin(CARGO_BIN)?;
+    cmd.arg("create")
+        .arg("db")
+        .arg("sample-db")
+        .arg("--dry-run");
+    cmd.assert()
+        .stdout(predicate::str::contains("kind: CoreDB"));
+
+    Ok(())
+}
