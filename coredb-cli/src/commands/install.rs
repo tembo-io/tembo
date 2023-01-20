5f761e84b0ccec18692edae1d95af4d5e759a430
Steven Miller <sjmiller609@gmail.com>
2023-01-20 16:20:33 -0500

    [COR-155] 'coredb install' to install operator

---
+++ coredb-cli/src/commands/install.rs
@@ -0,0 +1,25 @@
+use super::SubCommand;
+use clap::Args;
+use std::process::Command;
+
+#[derive(Args)]
+pub struct InstallCommand {}
+
+impl SubCommand for InstallCommand {
+    fn execute(&self) {
+        let output = Command::new("kubectl")
+            .arg("apply")
+            .arg("-f")
+            .arg("https://raw.githubusercontent.com/CoreDB-io/coredb/main/coredb-operator/yaml/crd.yaml")
+            .output()
+            .expect("Failed to execute 'kubectl' command.");
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+        let output = Command::new("kubectl")
+            .arg("apply")
+            .arg("-f")
+            .arg("https://raw.githubusercontent.com/CoreDB-io/coredb/main/coredb-operator/yaml/install.yaml")
+            .output()
+            .expect("Failed to execute 'kubectl' command.");
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+    }
+}
