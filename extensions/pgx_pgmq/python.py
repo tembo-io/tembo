5c0c523a521f7800b34f17e3210aa4571ed6785d
Adam Hendel <adam@coredb.io>
2023-02-06 23:02:15 -0600

    wip python example

---
+++ extensions/pgx_pgmq/python.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine, text
+
+import pandas as pd
+
+# Connect to the database
+engine = create_engine("postgresql://postgres:postgres@localhost:28814/pgx_pgmq")
+import json
+
+
+
+with engine.connect() as con:
+    # create a queue
+    created = con.execute(text( "select * from pgmq_create('myqueue');"))
+    # list queues
+    list_queues = con.execute(text( "select * from pgmq_list_queues()"))
+    column_names = list_queues.keys()
+    rows = list_queues.fetchall()
+    print("### Queues ###")
+    for row in rows:
+        print(dict(zip(column_names, row)))
+
+with engine.connect() as con:
+    # send a message
+    msg = json.dumps({"yolo": 42})
+    msg_id = con.execute(text(f"select * from pgmq_send('x', '{msg}') as msg_id;"))
+    column_names = msg_id.keys()
+    rows = msg_id.fetchall()
+    print("### Message ID ###")
+    for row in rows:
+        print(dict(zip(column_names, row)))
+    
+with engine.connect() as con:
+    # read a message, make it unavailable to be read again for 5 seconds
+    read = con.execute(text("select * from pgmq_read('x', 5);"))
+    column_names = read.keys()
+    rows = read.fetchall()
+    print("### Read Message ###")
+    for row in rows:
+        print(dict(zip(column_names, row)))
+
+with engine.connect() as con:
+    # delete a message
+    delete = con.execute(text("select pgmq_delete('x', 1);"))
+    column_names = delete.keys()
+    rows = delete.fetchall()
+    print("### Delete Message ###")
+    for row in rows:
+        print(dict(zip(column_names, row)))
