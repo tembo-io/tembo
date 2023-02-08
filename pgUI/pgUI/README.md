e55a1ae2a999d31834693cc24404ac0392134e2e
Adam Hendel <adam@coredb.io>
2023-02-07 21:38:23 -0600

    start pgUI

---
+++ pgUI/pgUI/README.md
@@ -0,0 +1,1 @@
+# Postgres UI
---
+++ pgUI/pgUI/components/Main/index.tsx
@@ -0,0 +1,32 @@
+import { CircularProgress, Stack } from '@mui/joy';
+
+import TopBar from '../TopBar';
+import LeftNavigation from '../LeftNav';
+
+import styles from './Main.module.scss';
+
+export default function Main(
+  props: React.PropsWithChildren & {
+    hasRightSidebar?: boolean;
+    allowCreation?: boolean;
+    entity?: string;
+  }
+) {
+  const { children, allowCreation, entity } = props;
+
+
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        width: '100%',
+      }}
+    >
+      <LeftNavigation allowCreation={allowCreation} entity={entity} />
+      <div className={styles.DashboardContainer}>
+        {<TopBar shortName='jon' />}
+        {children}
+      </div>
+    </Stack>
+  );
+}
