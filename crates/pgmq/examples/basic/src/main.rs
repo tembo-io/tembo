55bfaef429947f5f01ade1c85da48b103c6f98e8
Adam Hendel <adam@coredb.io>
2023-02-09 09:15:06 -0600

    examples lock file

--- crates/pgmq/examples/basic/src/main.rs
+++ crates/pgmq/examples/basic/src/main.rs
@@ -1,12 +1,12 @@
-use pgmq::{Message, PGMQueue};
+use pgmq::{errors::PgmqError, Message, PGMQueue};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), PgmqError> {
     // CREATE A QUEUE
     let queue: PGMQueue =
-        PGMQueue::new("postgres://postgres:postgres@0.0.0.0:5432".to_owned()).await;
+        PGMQueue::new("postgres://postgres:postgres@0.0.0.0:5432".to_owned()).await?;
     let myqueue = "myqueue".to_owned();
     queue
         .create(&myqueue)
@@ -17,10 +17,11 @@ async fn main() {
     let msg1 = serde_json::json!({
         "foo": "bar"
     });
-    let _msg_id1: i64 = queue
+    let msg_id1: i64 = queue
         .send(&myqueue, &msg1)
         .await
         .expect("Failed to enqueue message");
+    println!("Message ID: {msg_id1}");
 
     // SEND A STRUCT
     #[derive(Serialize, Debug, Deserialize)]
@@ -30,31 +31,38 @@ async fn main() {
     let msg2 = MyMessage {
         foo: "bar".to_owned(),
     };
-    let _msg_id2: i64 = queue
+    let msg_id2: i64 = queue
         .send(&myqueue, &msg2)
         .await
         .expect("Failed to enqueue message");
+    println!("Message ID: {msg_id2}");
 
     // READ A MESSAGE as `serde_json::Value`
     let vt: i32 = 30;
     let read_msg1: Message<Value> = queue
         .read::<Value>(&myqueue, Some(&vt))
-        .await
+        .await?
         .expect("no messages in the queue!");
+    println!("Message: {read_msg1:?}");
 
     // READ A MESSAGE as a struct
     let read_msg2: Message<MyMessage> = queue
         .read::<MyMessage>(&myqueue, Some(&vt))
-        .await
+        .await?
         .expect("no messages in the queue!");
+    println!("Message: {read_msg2:?}");
 
-    // DELETE THE MESSAGE WE SENT
+    // DELETE A MESSAGE WE SENT
     queue
         .delete(&myqueue, &read_msg1.msg_id)
         .await
         .expect("Failed to delete message");
+
+    // ARCHIVE THE OTHER MESSAGE
     queue
-        .delete(&myqueue, &read_msg2.msg_id)
+        .archive(&myqueue, &read_msg2.msg_id)
         .await
-        .expect("Failed to delete message");
+        .expect("Failed to archive message");
+
+    Ok(())
 }
