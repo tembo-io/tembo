60ffe1eadc9c48c490dccd3b24e819b39fa0baa7
Adam Hendel <adam@coredb.io>
2023-02-15 20:42:17 -0600

    remove background worker from test

--- extensions/pgmq/src/lib.rs
+++ extensions/pgmq/src/lib.rs
@@ -380,10 +380,14 @@ mod tests {
 pub mod pg_test {
     // pg_test module with both the setup and postgresql_conf_options functions are required
 
+    use std::vec;
+
     pub fn setup(_options: Vec<&str>) {}
 
     pub fn postgresql_conf_options() -> Vec<&'static str> {
         // return any postgresql.conf settings that are required for your tests
-        vec!["shared_preload_libraries = 'pg_partman_bgw'"]
+        // uncomment this when there are tests for the partman background worker
+        // vec!["shared_preload_libraries = 'pg_partman_bgw'"]
+        vec![]
     }
 }
