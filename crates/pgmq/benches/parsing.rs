1c5c52d65e25f316526e89ce93a1e4cc69b2aa8b
Adam Hendel <adam@coredb.io>
2023-02-17 12:35:44 -0600

    fix benchmarkjs

--- crates/pgmq/benches/parsing.rs
+++ crates/pgmq/benches/parsing.rs
@@ -1,16 +1,24 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use criterion::BenchmarkId;
+use lazy_static::lazy_static;
 use pgmq::query::check_input;
+use regex::Regex;
 
-fn check_bytes(input: &str) -> bool {}
+pub fn check_regex(input: &str) {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r#"^[a-zA-Z0-9_]+$"#).unwrap();
+    }
+    if !RE.is_match(input) {
+        panic!("Invalid queue name: {input}")
+    }
+}
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("check bytes", |b| {
-        b.iter(|| check_bytes(black_box("myqueue_123_longername")))
+        b.iter(|| check_input(black_box("myqueue_123_longername")))
     });
     c.bench_function("check regex", |b| {
-        b.iter(|| check_input(black_box("myqueue_123_longername")))
+        b.iter(|| check_regex(black_box("myqueue_123_longername")))
     });
 }
 
