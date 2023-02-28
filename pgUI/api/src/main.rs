5e3af0853d44829e792cca4882e489bf22031bee
Ian Stanton <ian@coredb.io>
2023-02-27 22:53:38 -0500

    Update bind address

--- pgUI/api/src/main.rs
+++ pgUI/api/src/main.rs
@@ -19,7 +19,7 @@ async fn main() -> std::io::Result<()> {
             .service(routes::connection)
             .service(get_queries)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
