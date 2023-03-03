6a19d0f9feb7725309039b89fb02dc776f86daa3
Ian Stanton <ian@coredb.io>
2023-03-03 17:02:21 -0500

    List extensions in S3 bucket

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use actix_web::{App, HttpServer};
-use extension_registry::routes;
-
-// description
+use actix_web::{web, App, HttpServer};
+use extension_registry::{config, routes};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // load configurations from environment
+    let cfg = config::S3Config::default();
     HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(cfg.clone()))
             .service(routes::running)
             .service(routes::get_extensions)
     })
@@ -14,6 +15,3 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
-
-// get all
-// respond with list of all extensions in some bucket (names, relevant metadata)
--- src/routes.rs
+++ src/routes.rs
@@ -1,4 +1,10 @@
-use actix_web::{get, HttpResponse, Responder};
+use crate::config::S3Config;
+use actix_web::{get, web, HttpResponse, Responder};
+use anyhow::Result;
+use s3::bucket::Bucket;
+use s3::creds::Credentials;
+use s3::error::S3Error;
+use s3::serde_types::{ListBucketResult, Object};
 
 #[get("/")]
 pub async fn running() -> impl Responder {
@@ -7,12 +13,21 @@ pub async fn running() -> impl Responder {
 
 // get all extensions in s3 bucket
 #[get("/get-extensions")]
-pub async fn get_extensions() -> impl Responder {
-    let mut extensions: Vec<String> = Vec::new();
-    // pass in s3 config
-    // return all extensions in s3 bucket
-    extensions.push("ext1".to_owned());
-    extensions.push("ext2".to_owned());
-    extensions.push("ext3".to_owned());
+pub async fn get_extensions(cfg: web::Data<S3Config>) -> impl Responder {
+    let mut extensions: Vec<&Object> = Vec::new();
+    let list: Result<Vec<ListBucketResult>, S3Error> = s3_list(&cfg.bucket_name, &cfg.region).await;
+    let ulist = list.unwrap();
+    for ext in ulist[0].contents.iter() {
+        extensions.push(ext);
+    }
     HttpResponse::Ok().body(format!("Extensions... {:?}", extensions))
+}
+
+pub async fn s3_list(bucket_name: &str, region: &str) -> Result<Vec<ListBucketResult>, S3Error> {
+    let region = region.parse()?;
+    // TODO(ianstanton) Allow for reading creds from env var
+    let credentials = Credentials::default()?;
+    let bucket = Bucket::new(bucket_name, region, credentials)?;
+    let list = bucket.list("".to_string(), Some("".to_string())).await?;
+    Ok::<Vec<ListBucketResult>, S3Error>(list)
 }
