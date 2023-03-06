222e8702e9a460bc6a2bc5ff38fc1a82794ab4a9
Steven Miller <sjmiller609@gmail.com>
2023-03-06 09:29:07 -0500

    pg_config path

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -3,12 +3,25 @@ use async_trait::async_trait;
 use clap::Args;
 
 #[derive(Args)]
-pub struct InstallCommand {}
+pub struct InstallCommand {
+    #[arg(long = "pg-config", short = 'p', default_value = "")]
+    pg_config: String,
+}
 
 #[async_trait]
 impl SubCommand for InstallCommand {
     async fn execute(&self) -> Result<(), anyhow::Error> {
-        println!("trunk install: not implemented");
+        let mut pg_config = String::new();
+        if self.pg_config == "" {
+            pg_config = which::which("pg_config")?
+                .into_os_string()
+                .into_string()
+                .unwrap();
+        } else {
+            // find pg_config in path
+            pg_config = self.pg_config.clone();
+        }
+        println!("Using pg_config: {}", pg_config);
         Ok(())
     }
 }
