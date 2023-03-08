9c83eef7c1688cdf49713913c26470b6ee61831a
Ian Stanton <ian@coredb.io>
2023-03-08 17:13:31 -0500

    Add migrations for extensions and versions tables

---
+++ migrations/2023-03-08-220203_versions/up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE versions (
+    id SERIAL4,
+    extension_id INT4,
+    num VARCHAR,
+    updated_at TIMESTAMP,
+    created_at TIMESTAMP,
+    downloads INT4,
+    features JSONB,
+    yanked BOOL,
+    license VARCHAR,
+    extension_size INT4,
+    published_by INT4,
+    checksum BPCHAR(64),
+    links VARCHAR
+);
