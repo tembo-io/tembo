91ee5d1d26c7369623dc42f7e146981b04559dbd
Adam Hendel <ChuckHend@users.noreply.github.com>
2023-03-13 09:39:03 -0500

    coredb-operator: add extensions to status (#157)

--- coredb-operator/src/extensions.rs
+++ coredb-operator/src/extensions.rs
@@ -1,7 +1,68 @@
-use crate::{Context, CoreDB, Error};
+use crate::{
+    controller::{Extension, ExtensionInstallLocation},
+    Context, CoreDB, Error,
+};
 use regex::Regex;
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 use tracing::{debug, info, warn};
+
+#[derive(Debug)]
+pub struct ExtRow {
+    pub name: String,
+    pub version: String,
+    pub enabled: bool,
+    pub schema: String,
+}
+
+const LIST_DATABASES_QUERY: &str = r#"SELECT datname FROM pg_database WHERE datistemplate = false;"#;
+const LIST_EXTENSIONS_QUERY: &str = r#"select
+distinct on
+(name) *
+from
+(
+select
+    *
+from
+    (
+    select
+        t0.extname as name,
+        t0.extversion as version,
+        true as enabled,
+        t1.nspname as schema
+    from
+        (
+        select
+            extnamespace,
+            extname,
+            extversion
+        from
+            pg_extension
+    ) t0,
+        (
+        select
+            oid,
+            nspname
+        from
+            pg_namespace
+    ) t1
+    where
+        t1.oid = t0.extnamespace
+) installed
+union
+select 
+    name,
+    default_version as version,
+    false as enabled,
+    'public' as schema
+from
+    pg_catalog.pg_available_extensions
+order by
+    enabled asc 
+) combined
+order by
+name asc,
+enabled desc
+"#;
 
 pub async fn manage_extensions(cdb: &CoreDB, ctx: Arc<Context>) -> Result<(), Error> {
     let client = ctx.client.clone();
@@ -65,4 +126,93 @@ pub async fn manage_extensions(cdb: &CoreDB, ctx: Arc<Context>) -> Result<(), Er
         }
     }
     Ok(())
+}
+
+pub async fn list_databases(cdb: &CoreDB, ctx: Arc<Context>) -> Result<Vec<String>, Error> {
+    let client = ctx.client.clone();
+    let psql_out = cdb
+        .psql(
+            LIST_DATABASES_QUERY.to_owned(),
+            "postgres".to_owned(),
+            client.clone(),
+        )
+        .await
+        .unwrap();
+    let result_string = psql_out.stdout.unwrap();
+    let mut databases = vec![];
+    for line in result_string.lines().skip(2) {
+        let fields: Vec<&str> = line.split('|').map(|s| s.trim()).collect();
+        if fields.is_empty() {
+            debug!("Done:{:?}", fields);
+            continue;
+        }
+        databases.push(fields[0].to_string());
+    }
+    let num_databases = databases.len();
+    info!("Found {} databases", num_databases);
+    Ok(databases)
+}
+
+pub async fn list_extensions(cdb: &CoreDB, ctx: Arc<Context>, database: &str) -> Result<Vec<ExtRow>, Error> {
+    let client = ctx.client.clone();
+    let psql_out = cdb
+        .psql(
+            LIST_EXTENSIONS_QUERY.to_owned(),
+            database.to_owned(),
+            client.clone(),
+        )
+        .await
+        .unwrap();
+    let result_string = psql_out.stdout.unwrap();
+    let mut extensions = vec![];
+    for line in result_string.lines().skip(2) {
+        let fields: Vec<&str> = line.split('|').map(|s| s.trim()).collect();
+        if fields.len() < 4 {
+            debug!("Done:{:?}", fields);
+            continue;
+        }
+        let package = ExtRow {
+            name: fields[0].to_owned(),
+            version: fields[1].to_owned(),
+            enabled: fields[2] == "t",
+            schema: fields[3].to_owned(),
+        };
+        extensions.push(package);
+    }
+    let num_extensions = extensions.len();
+    info!("Found {} extensions", num_extensions);
+    Ok(extensions)
+}
+
+/// list databases then get all extensions from each database
+pub async fn get_all_extensions(cdb: &CoreDB, ctx: Arc<Context>) -> Result<Vec<Extension>, Error> {
+    let databases = list_databases(cdb, ctx.clone()).await?;
+
+    let mut ext_hashmap: HashMap<String, Vec<ExtensionInstallLocation>> = HashMap::new();
+    // query every database for extensions
+    // transform results by extension name, rather than by database
+    for db in databases {
+        let extensions = list_extensions(cdb, ctx.clone(), &db).await?;
+        for ext in extensions {
+            let extlocation = ExtensionInstallLocation {
+                database: db.clone(),
+                version: Some(ext.version),
+                enabled: ext.enabled,
+                schema: ext.schema,
+            };
+            ext_hashmap
+                .entry(ext.name)
+                .or_insert_with(Vec::new)
+                .push(extlocation);
+        }
+    }
+
+    let mut ext_spec: Vec<Extension> = Vec::new();
+    for (ext_name, ext_locations) in &ext_hashmap {
+        ext_spec.push(Extension {
+            name: ext_name.clone(),
+            locations: ext_locations.clone(),
+        });
+    }
+    Ok(ext_spec)
 }
