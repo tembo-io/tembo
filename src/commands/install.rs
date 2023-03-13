be524c195fa0f819c1eced8aade4bf6ee63a2a90
Steven Miller <sjmiller609@gmail.com>
2023-03-13 09:39:34 -0400

    Configurable version when installing with coredb-cli (#159)

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -3,21 +3,28 @@ use clap::Args;
 use std::process::Command;
 
 #[derive(Args)]
-pub struct InstallCommand {}
+pub struct InstallCommand {
+    #[arg(short = 'b', long = "branch", default_value = "main")]
+    branch: String,
+}
 
 impl SubCommand for InstallCommand {
     fn execute(&self) {
         let output = Command::new("kubectl")
             .arg("apply")
             .arg("-f")
-            .arg("https://raw.githubusercontent.com/CoreDB-io/coredb/main/coredb-operator/yaml/crd.yaml")
+            .arg(
+                format!("https://raw.githubusercontent.com/CoreDB-io/coredb/{}/coredb-operator/yaml/crd.yaml", self.branch)
+                )
             .output()
             .expect("Failed to execute 'kubectl' command.");
         println!("{}", String::from_utf8_lossy(&output.stdout));
         let output = Command::new("kubectl")
             .arg("apply")
             .arg("-f")
-            .arg("https://raw.githubusercontent.com/CoreDB-io/coredb/main/coredb-operator/yaml/install.yaml")
+            .arg(
+                format!("https://raw.githubusercontent.com/CoreDB-io/coredb/{}/coredb-operator/yaml/install.yaml", self.branch)
+            )
             .output()
             .expect("Failed to execute 'kubectl' command.");
         println!("{}", String::from_utf8_lossy(&output.stdout));
