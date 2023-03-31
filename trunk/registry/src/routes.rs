698b4dd2ac0d28e473d0bd82cfd88bcf44cbd610
Ian Stanton <ian@coredb.io>
2023-03-31 14:33:48 -0400

    [COR-526] Improve JSON response for `/extensions/all` (#195)

--- trunk/registry/src/routes.rs
+++ trunk/registry/src/routes.rs
@@ -2,6 +2,8 @@ use crate::config::Config;
 use crate::connect;
 use crate::errors::ExtensionRegistryError;
 use actix_web::{get, web, HttpResponse, Responder};
+use serde_json::{json, Value};
+
 #[get("/")]
 pub async fn running() -> impl Responder {
     HttpResponse::Ok().body("API is up and running!")
@@ -11,7 +13,7 @@ pub async fn running() -> impl Responder {
 pub async fn get_all_extensions(
     cfg: web::Data<Config>,
 ) -> Result<HttpResponse, ExtensionRegistryError> {
-    let mut extensions: Vec<String> = Vec::new();
+    let mut extensions: Vec<Value> = Vec::new();
     // Set database conn
     let conn = connect(&cfg.database_url).await?;
     // Create a transaction on the database, if there are no errors,
@@ -21,8 +23,17 @@ pub async fn get_all_extensions(
         .fetch_all(&mut tx)
         .await?;
     for row in rows.iter() {
-        extensions.push(row.name.as_ref().unwrap().to_owned());
+        let data = json!(
+        {
+          "name": row.name.to_owned(),
+          "description": row.description.to_owned(),
+          "homepage": row.homepage.to_owned(),
+          "documentation": row.documentation.to_owned(),
+          "repository": row.repository.to_owned()
+        });
+        extensions.push(data);
     }
     // Return results in response
-    Ok(HttpResponse::Ok().body(format!("{:?}", extensions)))
+    let json = serde_json::to_string_pretty(&extensions)?;
+    Ok(HttpResponse::Ok().body(json))
 }
