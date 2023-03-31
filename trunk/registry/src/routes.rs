a9c679e7a2431108c996c98a6bfcff108fbe7c61
Ian Stanton <ian@coredb.io>
2023-03-31 12:26:56 -0400

    Prettify json response

--- trunk/registry/src/routes.rs
+++ trunk/registry/src/routes.rs
@@ -2,16 +2,8 @@ use crate::config::Config;
 use crate::connect;
 use crate::errors::ExtensionRegistryError;
 use actix_web::{get, web, HttpResponse, Responder};
-use serde::Serialize;
-
-#[derive(Debug, Serialize)]
-pub struct Extension {
-    name: Option<String>,
-    description: Option<String>,
-    homepage: Option<String>,
-    documentation: Option<String>,
-    repository: Option<String>,
-}
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 
 #[get("/")]
 pub async fn running() -> impl Responder {
@@ -22,7 +14,7 @@ pub async fn running() -> impl Responder {
 pub async fn get_all_extensions(
     cfg: web::Data<Config>,
 ) -> Result<HttpResponse, ExtensionRegistryError> {
-    let mut extensions: Vec<Extension> = Vec::new();
+    let mut extensions: Vec<Value> = Vec::new();
     // Set database conn
     let conn = connect(&cfg.database_url).await?;
     // Create a transaction on the database, if there are no errors,
@@ -32,16 +24,17 @@ pub async fn get_all_extensions(
         .fetch_all(&mut tx)
         .await?;
     for row in rows.iter() {
-        let ext = Extension {
-            name: row.name.to_owned(),
-            description: row.description.to_owned(),
-            homepage: row.homepage.to_owned(),
-            documentation: row.documentation.to_owned(),
-            repository: row.repository.to_owned(),
-        };
-        extensions.push(ext);
+        let data = json!(
+            {
+              "name": row.name.to_owned(),
+              "description": row.description.to_owned(),
+              "homepage": row.homepage.to_owned(),
+              "documentation": row.documentation.to_owned(),
+              "repository": row.repository.to_owned()
+            });
+        extensions.push(data);
     }
     // Return results in response
-    let json = serde_json::to_string(&extensions);
-    Ok(HttpResponse::Ok().body(format!("{:?}", json)))
+    let json = serde_json::to_string_pretty(&extensions)?;
+    Ok(HttpResponse::Ok().body(format!("{}", json)))
 }
