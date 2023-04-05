35d7b8db455fc5ea39b3833905f35fcb7c1df834
Adam Hendel <adam@coredb.io>
2023-04-05 16:38:39 -0500

    move chmod

--- postgres/install-extensions.sh
+++ postgres/install-extensions.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 #
 # This script is instead of 'trunk install'
-
-# give postgres group ownership of SHAREDIR and PKGLIBDIR for extensions
-chown -R :postgres  "$(pg_config --sharedir)" || :
-chmod -R 2775	"$(pg_config --sharedir)" || :
-chown -R :postgres "$(pg_config --pkglibdir)" || :
-chmod -R 2775	"$(pg_config --pkglibdir)" || :
-
 for extension in $(ls /extensions); do
   for file in $(tar -tvf /extensions/${extension} \
 		| rev | cut -d" " -f1 | rev | grep -E 'sql|so|control' | cut -d"/" -f2); do
