30492077a2c0f4d8e668051f937088b0ffd812d5
Ian Stanton <ian@coredb.io>
2023-04-06 18:00:51 -0400

    [COR-564] Add latest version and timestamps to `/extensions/all` (#212)

--- trunk/registry/src/download.rs
+++ trunk/registry/src/download.rs
@@ -1,7 +1,9 @@
 //! Functionality for downloading extensions and maintaining download counts
 use crate::config::Config;
+use crate::errors::ExtensionRegistryError;
 use crate::uploader::extension_location;
 use actix_web::{get, web, HttpResponse, Responder};
+use sqlx::{Pool, Postgres};
 
 /// Handles the `GET /extensions/:extension_name/:version/download` route.
 /// This returns a URL to the location where the extension is stored.
@@ -12,4 +14,21 @@ pub async fn download(cfg: web::Data<Config>, path: web::Path<(String, String)>)
     // TODO(ianstanton) Use latest version if none provided
     let url = extension_location(&cfg.bucket_name, &name, &version);
     HttpResponse::Ok().body(url)
+}
+
+pub async fn latest_version(
+    extension_name: &str,
+    conn: web::Data<Pool<Postgres>>,
+) -> Result<String, ExtensionRegistryError> {
+    // Create a transaction on the database, if there are no errors,
+    // commit the transactions to record a new or updated extension.
+    let mut tx = conn.begin().await?;
+    let ext = sqlx::query!("SELECT * FROM extensions WHERE name = $1", extension_name)
+        .fetch_one(&mut tx)
+        .await?;
+    let id: i32 = ext.id as i32;
+    let latest = sqlx::query!("SELECT MAX(num) FROM versions WHERE extension_id = $1;", id)
+        .fetch_one(&mut tx)
+        .await?;
+    Ok(latest.max.unwrap())
 }
