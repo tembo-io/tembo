8ddb75dd432843dc04d2634d1eded2e299da2e26
Ian Stanton <ian@coredb.io>
2023-04-07 10:57:45 -0400

    Read default local file if none specified

--- trunk/cli/src/commands/publish.rs
+++ trunk/cli/src/commands/publish.rs
@@ -37,38 +37,55 @@ pub enum PublishError {
 #[async_trait]
 impl SubCommand for PublishCommand {
     async fn execute(&self, _task: Task) -> Result<(), anyhow::Error> {
-        if let Some(ref file) = self.file {
-            check_input(&self.name)?;
-            let mut headers = HeaderMap::new();
-            headers.insert(CONTENT_TYPE, "application/octet-stream".parse().unwrap());
-            let file = fs::read(file).unwrap();
-            let file_part = reqwest::multipart::Part::bytes(file)
-                .file_name("pgmq-0.2.1.tar.gz")
-                .headers(headers);
-            let m = json!({
-                "name": self.name,
-                "vers": self.version,
-                "description": self.description,
-                "documentation": self.documentation,
-                "license": self.license,
-                "repository": self.repository
-            });
-            let metadata = reqwest::multipart::Part::text(m.to_string());
-            let form = reqwest::multipart::Form::new()
-                .part("metadata", metadata)
-                .part("file", file_part);
-            let client = reqwest::Client::new();
-            let url = format!("{}/extensions/new", self.registry);
-            let res = client
-                .post(url)
-                .multipart(form)
-                .send()
-                .await?
-                .text()
-                .await?;
-            // print response from registry
-            println!("{}", res)
-        }
+        // Validate extension name input
+        check_input(&self.name)?;
+        let (file, name) = match &self.file {
+            Some(..) => {
+                // If file is specified, use it
+                let path = self.file.clone().unwrap();
+                let name = path.file_name().unwrap().to_str().unwrap().to_owned();
+                let f = fs::read(self.file.clone().unwrap())?;
+                (f, name)
+            }
+            None => {
+                // If no file is specified, read file from working dir with format
+                // <extension_name>-<version>.tar.gz.
+                // Error if file is not found
+                let mut path = PathBuf::new();
+                let _ = &path.push(format!("./{}-{}.tar.gz", self.name, self.version));
+                let name = path.file_name().unwrap().to_str().unwrap().to_owned();
+                let f = fs::read(path.clone())?;
+                (f, name)
+            }
+        };
+        let mut headers = HeaderMap::new();
+        headers.insert(CONTENT_TYPE, "application/octet-stream".parse().unwrap());
+        let file_part = reqwest::multipart::Part::bytes(file)
+            .file_name(name)
+            .headers(headers);
+        let m = json!({
+            "name": self.name,
+            "vers": self.version,
+            "description": self.description,
+            "documentation": self.documentation,
+            "license": self.license,
+            "repository": self.repository
+        });
+        let metadata = reqwest::multipart::Part::text(m.to_string());
+        let form = reqwest::multipart::Form::new()
+            .part("metadata", metadata)
+            .part("file", file_part);
+        let client = reqwest::Client::new();
+        let url = format!("{}/extensions/new", self.registry);
+        let res = client
+            .post(url)
+            .multipart(form)
+            .send()
+            .await?
+            .text()
+            .await?;
+        // Print response from registry
+        println!("{}", res);
         Ok(())
     }
 }
