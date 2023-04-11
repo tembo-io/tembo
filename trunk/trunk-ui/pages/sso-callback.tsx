4b7fdcea8ce12582767ce7bc6bbb88fe61887b25
Karl Morand <karljmorand@gmail.com>
2023-04-11 12:32:41 -0400

    more cleanup

--- trunk/trunk-ui/pages/sso-callback.tsx
+++ trunk/trunk-ui/pages/sso-callback.tsx
@@ -1,10 +1,5 @@
 import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
-import { useUser, useSignIn, useClerk } from "@clerk/nextjs";
 
 export default function SSOCallBack() {
-  const { user } = useUser();
-
-  console.log("SSO CALLBACK", user);
-
   return <AuthenticateWithRedirectCallback />;
 }
