580cedbe68d57a85eadc48e76daa0ebd6391ea3e
Karl Morand <karljmorand@gmail.com>
2023-04-11 12:19:59 -0400

    add link to footer

--- trunk/trunk-ui/components/Footer/index.tsx
+++ trunk/trunk-ui/components/Footer/index.tsx
@@ -10,10 +10,12 @@ const inter = Inter({ subsets: ["latin"], weight: ["800", "400"] });
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <p className={cx(inter.className, styles.text)}>Sponsored by</p>
-      <Image className={styles.image} src={Logo} alt="Logo" />
-      <Image className={styles.image} src={CoreDB} alt="Logo" />
+    <footer>
+      <a href="https://coredb.io/" className={styles.footer}>
+        <p className={cx(inter.className, styles.text)}>Sponsored by</p>
+        <Image className={styles.image} src={Logo} alt="Logo" />
+        <Image className={styles.image} src={CoreDB} alt="Logo" />
+      </a>
     </footer>
   );
 }
