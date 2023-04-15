ac0bfcc77ef07b86d6db74b4ba53718e38ebae96
Adam Hendel <adam@coredb.io>
2023-04-15 11:23:48 -0500

    wip spi

--- examples/create-extension/my_extension/src/lib.rs
+++ examples/create-extension/my_extension/src/lib.rs
@@ -1,4 +1,6 @@
 use pgx::prelude::*;
+use pgx::spi::SpiTupleTable;
+use pgx::log;
 
 pgx::pg_module_magic!();
 
@@ -7,7 +9,28 @@ fn hello_my_extension() -> &'static str {
     "Hello, my_extension"
 }
 
+// drop extension my_extension; create extension my_extension;
+// select * from list_extensions();
 
+// tail -f ~/.pgx/15.log
+
+
+extension_sql!(
+    r#"
+
+CREATE TABLE spi_example (
+    id serial8 not null primary key,
+    title text
+);
+
+INSERT INTO spi_example (title) VALUES ('This is a test');
+INSERT INTO spi_example (title) VALUES ('Hello There!');
+INSERT INTO spi_example (title) VALUES ('I like pudding');
+
+
+"#,
+    name = "create_sqi_example_table",
+);
 
 
 #[pg_extern]
@@ -15,27 +38,42 @@ fn list_extensions() -> Result<
 TableIterator<
     'static,
     (
-        name!(name, String),
-        name!(default_version, String),
-        name!(installed_version, String),
-        name!(comment, String),
+        name!(name, Option<String>),
+        name!(default_version, Option<String>),
+        name!(installed_version, Option<String>),
+        name!(comment, Option<String>),
     ),
 >,
 spi::Error,
 > {
-    let query = "select name, default_version, installed_version, comment from pg_available_extensions;";
-    let results: Result<Vec<(String, String, String, String)>, spi::Error> = Spi::connect(|client| {
-        Ok(client.select(query, None, None)?.map(|row| (
-            row["name"].value::<String>(),
-            row["default_version"].value::<String>(),
-            row["installed_version"].value::<String>(),
-            row["comment"].value::<String>(),
-        ))
-        )
+    let results : Result<Vec<(
+        Option<String>,
+        Option<String>,
+        Option<String>,
+        Option<String>
+    )>, spi::Error> = Spi::connect(|mut client| {
+        let mut results: Vec<(
+            Option<String>,
+            Option<String>,
+            Option<String>,
+            Option<String>
+        )> = Vec::new();
+        let query = "select * from pg_catalog.pg_available_extensions".to_owned();
+        let tup_table: SpiTupleTable = client.update(&query, None, None)?;
+        log!("tup_table: {:?}", tup_table);
+        for row in tup_table.into_iter() {
+            log!("row:");
+            let name = row["name"].value::<String>()?;
+            println!("name: {}", name.clone().unwrap());
+            let default_version = row["default_version"].value::<String>()?;
+            let installed_version = row["installed_version"].value::<String>()?;
+            let comment = row["comment"].value::<String>()?;
+            results.push((name, default_version, installed_version, comment));
+        }
+        Ok(results)
     });
     Ok(TableIterator::new(results?.into_iter()))
 }
-
 
 
 #[cfg(any(test, feature = "pg_test"))]
