a414a5c6a8769623d65169f2eb2708bf2e795943
Ian Stanton <ian@coredb.io>
2023-04-27 08:30:54 -0400

    Fix test

--- coredb-cli/tests/integration_tests.rs
+++ coredb-cli/tests/integration_tests.rs
@@ -18,8 +18,10 @@ fn help() -> Result<(), Box<dyn std::error::Error>> {
 fn create_dry_run() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin(CARGO_BIN)?;
     cmd.arg("create")
+        .arg("--resource-type")
         .arg("db")
         .arg("--dry-run")
+        .arg("--name")
         .arg("sample-db");
     cmd.assert()
         .stdout(predicate::str::contains("kind: CoreDB"));
@@ -27,14 +29,18 @@ fn create_dry_run() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin(CARGO_BIN)?;
     cmd.arg("create")
         .arg("--dry-run")
+        .arg("--resource-type")
         .arg("db")
+        .arg("--name")
         .arg("sample-db");
     cmd.assert()
         .stdout(predicate::str::contains("kind: CoreDB"));
 
     let mut cmd = Command::cargo_bin(CARGO_BIN)?;
     cmd.arg("create")
+        .arg("--resource-type")
         .arg("db")
+        .arg("--name")
         .arg("sample-db")
         .arg("--dry-run");
     cmd.assert()
