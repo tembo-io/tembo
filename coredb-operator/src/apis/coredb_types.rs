86a5f1d264917a8cbf61bb9af35b45c690a171d2
Nick Hudson <nick.hudson@gmail.com>
2023-05-10 11:49:28 -0500

    [coredb-operator] adding in new CoreDB spec object for backups (#316)

--- coredb-operator/src/apis/coredb_types.rs
+++ coredb-operator/src/apis/coredb_types.rs
@@ -15,6 +15,18 @@ pub struct ServiceAccountTemplate {
     pub metadata: Option<ObjectMeta>,
 }
 
+#[derive(Deserialize, Serialize, Clone, Debug, JsonSchema, Default)]
+#[allow(non_snake_case)]
+pub struct Backup {
+    pub destinationPath: Option<String>,
+    #[serde(default = "defaults::default_encryption")]
+    pub encryption: Option<String>,
+    #[serde(default = "defaults::default_retention_policy")]
+    pub retentionPolicy: Option<String>,
+    #[serde(default = "defaults::default_backup_schedule")]
+    pub schedule: Option<String>,
+}
+
 /// Generate the Kubernetes wrapper struct `CoreDB` from our Spec and Status struct
 ///
 /// This provides a hook for generating the CRD yaml (in crdgen.rs)
@@ -26,30 +38,45 @@ pub struct ServiceAccountTemplate {
 pub struct CoreDBSpec {
     #[serde(default = "defaults::default_replicas")]
     pub replicas: i32,
+
     #[serde(default = "defaults::default_resources")]
     pub resources: ResourceRequirements,
+
     #[serde(default = "defaults::default_storage")]
     pub storage: Quantity,
+
     #[serde(default = "defaults::default_sharedir_storage")]
     pub sharedirStorage: Quantity,
+
     #[serde(default = "defaults::default_pkglibdir_storage")]
     pub pkglibdirStorage: Quantity,
+
     #[serde(default = "defaults::default_postgres_exporter_enabled")]
     pub postgresExporterEnabled: bool,
+
     #[serde(default = "defaults::default_image")]
     pub image: String,
+
     #[serde(default = "defaults::default_postgres_exporter_image")]
     pub postgresExporterImage: String,
+
     #[serde(default = "defaults::default_port")]
     pub port: i32,
+
     #[serde(default = "defaults::default_uid")]
     pub uid: i32,
+
     #[serde(default = "defaults::default_extensions")]
     pub extensions: Vec<Extension>,
+
     #[serde(default = "defaults::default_stop")]
     pub stop: bool,
+
     #[serde(default = "defaults::default_service_account_template")]
     pub serviceAccountTemplate: ServiceAccountTemplate,
+
+    #[serde(default = "defaults::default_backup")]
+    pub backup: Option<Backup>,
 }
 
 /// The status object of `CoreDB`
