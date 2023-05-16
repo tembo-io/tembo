642850ca51b3eb2705d5a52e494be754f8ce21e1
Nick Hudson <nick.hudson@gmail.com>
2023-05-16 16:22:51 -0500

    update default images, make sure image is updated from coredb

--- coredb-operator/src/defaults.rs
+++ coredb-operator/src/defaults.rs
@@ -38,7 +38,7 @@ pub fn default_port() -> i32 {
 }
 
 pub fn default_image() -> String {
-    "quay.io/coredb/coredb-pg-slim:7f0983b".to_owned()
+    "quay.io/coredb/coredb-pg-slim:e7a4faa".to_owned()
 }
 
 pub fn default_storage() -> Quantity {
@@ -54,7 +54,7 @@ pub fn default_pkglibdir_storage() -> Quantity {
 }
 
 pub fn default_postgres_exporter_image() -> String {
-    "quay.io/prometheuscommunity/postgres-exporter:v0.11.1".to_owned()
+    "quay.io/prometheuscommunity/postgres-exporter:v0.12.0".to_owned()
 }
 
 pub fn default_extensions() -> Vec<Extension> {
