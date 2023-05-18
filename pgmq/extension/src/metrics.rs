fd1946d8b5b32de787dda6c807231ef1e6bc3d8a
Adam Hendel <adam@coredb.io>
2023-05-17 22:27:25 -0500

    fix timestamp type

--- pgmq/extension/src/metrics.rs
+++ pgmq/extension/src/metrics.rs
@@ -7,7 +7,7 @@ use pgrx::warning;
 use crate::api::listit;
 use pgmq_crate::query::TABLE_PREFIX;
 
-type MetricResult = Vec<(String, i64, Option<i32>, Option<i32>, Timestamp)>;
+type MetricResult = Vec<(String, i64, Option<i32>, Option<i32>, TimestampWithTimeZone)>;
 
 #[pg_extern]
 fn pgmq_metrics(
@@ -20,7 +20,7 @@ fn pgmq_metrics(
             name!(queue_length, i64),
             name!(newest_msg_age_sec, Option<i32>),
             name!(oldest_msg_age_sec, Option<i32>),
-            name!(scrape_time, Timestamp),
+            name!(scrape_time, TimestampWithTimeZone),
         ),
     >,
     crate::PgmqExtError,
@@ -38,7 +38,7 @@ fn pgmq_metrics_all() -> Result<
             name!(queue_length, i64),
             name!(newest_msg_age_sec, Option<i32>),
             name!(oldest_msg_age_sec, Option<i32>),
-            name!(scrape_time, Timestamp),
+            name!(scrape_time, TimestampWithTimeZone),
         ),
     >,
     crate::PgmqExtError,
@@ -53,17 +53,17 @@ fn pgmq_metrics_all() -> Result<
 }
 
 fn query_summary(queue_name: &str) -> Result<MetricResult, crate::PgmqExtError> {
-    let query: String = build_summary_query(&queue_name);
+    let query: String = build_summary_query(queue_name);
     let results: Result<MetricResult, crate::PgmqExtError> = Spi::connect(|client| {
         let mut results: MetricResult = Vec::new();
-        let mut tup_table: SpiTupleTable = client.select(&query, None, None)?;
-        while let Some(row) = tup_table.next() {
+        let tup_table: SpiTupleTable = client.select(&query, None, None)?;
+        for row in tup_table {
             let queue_name = queue_name.to_owned();
             let queue_length = row["queue_length"].value::<i64>()?.expect("no msg_id");
             let newest_msg_sec = row["newest_msg_age_sec"].value::<i32>()?;
             let oldest_msg_sec = row["oldest_msg_age_sec"].value::<i32>()?;
-            let scrape_time = row["scrape_time_utc"]
-                .value::<Timestamp>()?
+            let scrape_time = row["scrape_time"]
+                .value::<TimestampWithTimeZone>()?
                 .expect("scrape timestamp missing");
             results.push((
                 queue_name,
@@ -90,7 +90,7 @@ fn build_summary_query(queue_name: &str) -> String {
             count(*) as queue_length,
             (EXTRACT(epoch FROM (SELECT (NOW() at time zone 'utc' -  max(enqueued_at)))))::int as newest_msg_age_sec,
             (EXTRACT(epoch FROM (SELECT (NOW() at time zone 'utc' -  min(enqueued_at)))))::int as oldest_msg_age_sec,
-            (NOW() at time zone 'utc') as scrape_time_utc
+            (NOW() at time zone 'utc')::timestamp at time zone 'utc' as scrape_time
         FROM {TABLE_PREFIX}_{queue_name};
         "
     )
