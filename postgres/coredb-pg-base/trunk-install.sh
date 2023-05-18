d66f0958683f71b4b93364de489ae5f04a677204
Ian Stanton <ian@coredb.io>
2023-05-17 21:16:50 -0400

    Untrack trunk-install.sh

--- postgres/coredb-pg-base/trunk-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-file='extensions.txt'
-lines=$(cat $file)
-for line in $lines
-do
-	echo $line
-	trunk install $line --pg-config /usr/lib/postgresql/15/bin/pg_config
-	psql -c "create extension \"$line\";"
-done
