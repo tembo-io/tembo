6cf666529a06099a3b737fed99f79f262e1fe938
Adam Hendel <adam@coredb.io>
2023-05-19 16:10:49 -0500

    add a setter

--- coredb-operator/src/configmap.rs
+++ coredb-operator/src/configmap.rs
@@ -1,28 +1,84 @@
-use crate::{apis::coredb_types::CoreDB, defaults, patch_cdb_status_merge, Context, Error};
+use crate::Error;
 use k8s_openapi::api::core::v1::ConfigMap;
 use kube::{
-    api::{Api, ObjectMeta, PostParams, Resource},
+    api::{Api, ObjectMeta, Patch, PatchParams, PostParams},
     Client,
 };
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-use std::{collections::BTreeMap, sync::Arc};
-use tracing::{debug, error, info, warn};
+use std::collections::BTreeMap;
+
+use tracing::{debug, error, info};
 
 
-pub async fn create_configmap(client: Client, namespace: &str) -> Result<(), Error> {
-    let configmaps: Api<ConfigMap> = Api::namespaced(client, namespace);
+pub async fn create_configmap_ifnotexist(
+    client: Client,
+    namespace: &str,
+    cm_name: &str,
+) -> Result<(), Error> {
+    let cm_api: Api<ConfigMap> = Api::namespaced(client, namespace);
 
+    match cm_api.get(cm_name).await {
+        Ok(o) => {
+            debug!("Configmap {} already exists", o.metadata.name.unwrap());
+            return Ok(());
+        }
+        Err(e) => {
+            create_configmap(cm_api, cm_name).await?;
+            return Ok(());
+        }
+    };
+}
+
+pub async fn create_configmap(cm_api: Api<ConfigMap>, cm_name: &str) -> Result<(), Error> {
     let cm = ConfigMap {
         metadata: ObjectMeta {
-            name: Some("prom-pg-queries".to_string()),
+            name: Some(cm_name.to_string()),
             ..Default::default()
         },
         ..Default::default()
     };
 
     let pp = PostParams::default();
-    match configmaps.create(&pp, &cm).await {
+    match cm_api.create(&pp, &cm).await {
+        Ok(o) => {
+            info!("Created empty configmap: {}", o.metadata.name.unwrap());
+        }
+        Err(e) => {
+            error!("Failed to create empty configmap: {}", e);
+        }
+    };
+    Ok(())
+}
+
+pub async fn set_configmap(
+    client: Client,
+    namespace: &str,
+    cm_name: &str,
+    data: BTreeMap<String, String>,
+) -> Result<(), Error> {
+    let cm_api: Api<ConfigMap> = Api::namespaced(client, namespace);
+    let cm = ConfigMap {
+        metadata: ObjectMeta {
+            name: Some(cm_name.to_string()),
+            ..Default::default()
+        },
+        data: Some(data),
+        ..Default::default()
+    };
+
+    let patch_params = PatchParams::apply("myapp");
+    let patch = Patch::Apply(&cm);
+
+    match cm_api.patch(cm_name, &patch_params, &patch).await {
+        Ok(o) => {
+            println!("Updated configmap: {}", o.metadata.name.unwrap());
+        }
+        Err(e) => {
+            eprintln!("Failed to update configmap: {}", e);
+        }
+    };
+
+    let pp = PostParams::default();
+    match cm_api.create(&pp, &cm).await {
         Ok(o) => {
             info!("Created empty configmap: {}", o.metadata.name.unwrap());
         }
