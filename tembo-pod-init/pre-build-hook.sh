067d85b68ba7282db554663bbecf57a536a64143
Steven Miller <sjmiller609@gmail.com>
2023-07-24 12:39:13 -0400

    Fix tembo-pod-init build

--- tembo-pod-init/pre-build-hook.sh
+++ tembo-pod-init/pre-build-hook.sh
@@ -1,4 +1,4 @@
-# brings coredb-operator into the build context
+# brings operator into the build context
 # intended to be used as pre-build-hook for the Tembo build github action
 # https://github.com/tembo-io/tembo/blob/main/.github/actions/build-and-push-to-quay/action.yml
-cp -r ../coredb-operator ./
+cp -r ../tembo-operator ./
