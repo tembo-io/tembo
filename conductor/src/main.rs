864188e9b7d3f04967064200f000d3bc9d6b54aa
Vinícius Miguel <36349314+vrmiguel@users.noreply.github.com>
2023-10-02 12:43:55 -0300

    conductor: requeue restart event if cnpg is still restarting (#298)

--- conductor/src/main.rs
+++ conductor/src/main.rs
@@ -439,7 +439,20 @@ async fn run(metrics: CustomMetrics) -> Result<(), Box<dyn std::error::Error>> {
                         let status = coredb.status.as_ref().unwrap();
                         if !status.running {
                             // Instance is still rebooting, recheck this message later
-                            info!("{}: Instance is still rebooting", read_msg.msg_id);
+                            let _ = queue
+                                .set_vt::<CRUDevent>(
+                                    &control_plane_events_queue,
+                                    read_msg.msg_id,
+                                    REQUEUE_VT_SEC_SHORT,
+                                )
+                                .await?;
+                            metrics.conductor_requeues.add(
+                                &opentelemetry::Context::current(),
+                                1,
+                                &[KeyValue::new("queue_duration", "short")],
+                            );
+
+                            continue;
                         }
 
                         let as_json = serde_json::to_string(&coredb);
