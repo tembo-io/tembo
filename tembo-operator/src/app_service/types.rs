74de8f5e8e3487755727116c7fbcca3b716dc7c4
Evan Stanton <evan.h.stanton@gmail.com>
2023-12-06 11:22:15 -0500

    Add new spec value ingress_type.

--- tembo-operator/src/app_service/types.rs
+++ tembo-operator/src/app_service/types.rs
@@ -79,6 +79,21 @@ pub struct Routing {
     #[serde(rename = "entryPoints")]
     #[serde(default = "default_entry_points")]
     pub entry_points: Option<Vec<String>>,
+    #[serde(rename = "ingressType")]
+    #[serde(default = "default_ingress_type")]
+    pub ingress_type: Option<IngressType>,
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize, ToSchema, JsonSchema)]
+pub enum IngressType {
+    http,
+    tcp,
+    udp,
+}
+
+pub fn default_ingress_type() -> Option<IngressType> {
+    Some(IngressType::http)
 }
 
 pub fn default_entry_points() -> Option<Vec<String>> {
