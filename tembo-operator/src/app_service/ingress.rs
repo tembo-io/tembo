6b5f9b1d21650f01dbf7f13d493946b467cd5d3a
Evan Stanton <evan.h.stanton@gmail.com>
2023-12-15 14:59:58 +0100

    Update matcher.

--- tembo-operator/src/app_service/ingress.rs
+++ tembo-operator/src/app_service/ingress.rs
@@ -254,7 +254,6 @@ pub fn generate_ingress_tcp_routes(
     appsvc: &AppService,
     resource_name: &str,
     namespace: &str,
-    host_matcher: String,
     coredb_name: &str,
 ) -> Option<Vec<IngressRouteTCPRoutes>> {
     match appsvc.routing.clone() {
@@ -269,7 +268,7 @@ pub fn generate_ingress_tcp_routes(
                             continue;
                         }
 
-                        let matcher = format!("{host_matcher} && PathPrefix(`{}`)", path);
+                        let matcher = format!("HostSNI(`{}`)", path);
                         let middlewares: Option<Vec<IngressRouteTCPRoutesMiddlewares>> =
                             route.middlewares.clone().map(|names| {
                                 names
@@ -398,9 +397,10 @@ pub async fn reconcile_ingress_tcp(
     desired_routes: Vec<IngressRouteTCPRoutes>,
     desired_middlewares: Vec<Middleware>,
     entry_points_tcp: Vec<String>,
+    app_name: &str,
 ) -> Result<(), kube::Error> {
     let ingress_api: Api<IngressRouteTCP> = Api::namespaced(client.clone(), ns);
-    let name = format!("{}-apps", coredb_name);
+    let name = format!("{}-{}", coredb_name, app_name);
 
     let middleware_api: Api<TraefikMiddleware> = Api::namespaced(client.clone(), ns);
     let desired_middlewares = generate_middlewares(&name, ns, oref.clone(), desired_middlewares);
