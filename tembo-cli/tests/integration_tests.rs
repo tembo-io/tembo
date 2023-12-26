717f1b44b8d083b987651d8eacc58a6b4d4d247f
Steven Miller <sjmiller609@gmail.com>
2023-12-26 16:28:12 -0500

    Add test for minimal

--- tembo-cli/tests/integration_tests.rs
+++ tembo-cli/tests/integration_tests.rs
@@ -1,6 +1,13 @@
+use std::env;
+use std::error::Error;
+use std::path::PathBuf;
 use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
-use std::process::Command; // Run programs
+use std::process::Command;
+use tokio_postgres::{Client, NoTls, Config};
+use tokio_postgres::config::SslMode;
+use postgres_openssl::MakeTlsConnector;
+use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 
 const CARGO_BIN: &str = "tembo";
 
@@ -14,22 +21,57 @@ fn help() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-/*
-#[test]
-fn init() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin(CARGO_BIN)?;
-    cmd.arg("init");
+#[tokio::test]
+async fn minimal() ->  Result<(), Box<dyn Error>> {
 
-    cmd.assert()
-        .stdout(predicate::str::contains("Tembo home directory created"));
-    cmd.assert()
-        .stdout(predicate::str::contains("Tembo context file created"));
-    cmd.assert()
-        .stdout(predicate::str::contains("Tembo config file created"));
-    cmd.assert().stdout(predicate::str::contains(
-        "Tembo migrations directory created",
-    ));
+    // Get the root directory of the crate
+    let root_dir = env!("CARGO_MANIFEST_DIR");
+    let test_dir = PathBuf::from(root_dir)
+        .join("tests")  // Adjust the path to the tests directory
+        .join("tomls")
+        .join("minimal");
+
+    // Change the current working directory
+    env::set_current_dir(&test_dir)?;
+
+    // Execute the command
+    let mut cmd = Command::cargo_bin(CARGO_BIN)?;
+    cmd.arg("apply");
+    cmd.assert().success();
+
+
+    // PostgreSQL connection string
+    let mut config = Config::new();
+    config.host("localhost");
+    config.user("postgres");
+    config.password("postgres");
+    config.dbname("postgres");
+    config.port(5432);
+    config.ssl_mode(SslMode::Require); // Set SSL mode to "require"
+
+    let mut builder = SslConnector::builder(SslMethod::tls()).expect("unable to create sslconnector builder");
+    builder.set_verify(SslVerifyMode::NONE);
+    let connector = MakeTlsConnector::new(builder.build());
+
+    // Connect to the PostgreSQL database
+    let (client, connection) = config.connect(connector).await?;
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    // Execute a simple query
+    let rows = client.query("SELECT 1", &[]).await?;
+
+    // Check that the query returned exactly one row with one column
+    assert_eq!(rows.len(), 1);
+    let value: i32 = rows[0].get(0);
+    assert_eq!(value, 1, "Query did not return 1");
 
     Ok(())
 }
- */
+
