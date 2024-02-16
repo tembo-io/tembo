d45fda609e7ae294ad897beb2bc52ef16bb93cde
Adarsh Shah <github@shahadarsh.com>
2024-02-16 16:58:37 -0500

    uses sqlx query instead of migrations for creating extensions (#551)

--- tembo-cli/src/cli/sqlx_utils.rs
+++ tembo-cli/src/cli/sqlx_utils.rs
@@ -2,6 +2,7 @@ use crate::tui::colors;
 use colorful::{Color, Colorful};
 use spinoff::{spinners, Spinner};
 use sqlx::migrate::Migrator;
+use sqlx::postgres::PgConnectOptions;
 use sqlx::Pool;
 use sqlx::Postgres;
 use std::path::Path;
@@ -10,38 +11,16 @@ use temboclient::models::ConnectionInfo;
 pub struct SqlxUtils {}
 
 impl SqlxUtils {
-    // run sqlx migrate
-    pub async fn run_migrations(
-        connection_info: ConnectionInfo,
-        instance_name: String,
-    ) -> Result<(), anyhow::Error> {
-        let mut sp = Spinner::new(
-            spinners::Dots,
-            "Running SQL migration",
-            spinoff::Color::White,
-        );
+    pub async fn execute_sql(instance_name: String, sql: String) -> Result<(), anyhow::Error> {
+        let connect_options = PgConnectOptions::new()
+            .username("postgres")
+            .password("postgres")
+            .host(&format!("{}.local.tembo.io", instance_name))
+            .database("postgres");
 
-        let connection_string = format!(
-            "postgresql://{}:{}@{}:{}",
-            connection_info.user,
-            connection_info.password,
-            connection_info.host,
-            connection_info.port
-        );
+        let pool = sqlx::PgPool::connect_with(connect_options).await?;
 
-        let pool = Pool::<Postgres>::connect(connection_string.as_str()).await?;
-
-        let path = instance_name.clone() + "/migrations";
-        let m = Migrator::new(Path::new(&path)).await?;
-        m.run(&pool).await?;
-
-        sp.stop_with_message(&format!(
-            "{} {}",
-            "✓".color(colors::indicator_good()).bold(),
-            format!("SQL migration completed for {}", instance_name)
-                .color(Color::White)
-                .bold()
-        ));
+        sqlx::query(&sql).execute(&pool).await?;
 
         Ok(())
     }
