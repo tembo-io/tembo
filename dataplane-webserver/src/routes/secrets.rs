ca38483061d2fe80dea617e0acfb83ab43d0339c
Joshua Jerin <joshuaxjerin@gmail.com>
2024-04-04 03:48:48 +0530

    checking admin role

--- dataplane-webserver/src/routes/secrets.rs
+++ dataplane-webserver/src/routes/secrets.rs
@@ -4,6 +4,7 @@ use crate::{config, secrets};
 use actix_web::error::ErrorInternalServerError;
 use actix_web::{get, patch, web, Error, HttpRequest, HttpResponse};
 use base64::prelude::*;
+use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
 use k8s_openapi::api::core::v1::Namespace;
 use k8s_openapi::api::core::v1::Secret;
 use kube::api::ListParams;
@@ -12,7 +13,10 @@ use kube::{Api, Client};
 use lazy_static::lazy_static;
 use log::{error, warn};
 use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::ops::Deref;
+use std::str;
 
 lazy_static! {
     pub static ref SECRETS_ALLOW_LIST: Vec<AvailableSecret> = {
@@ -48,6 +52,11 @@ lazy_static! {
         );
         secrets_allow_list
     };
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Claims {
+    organizations: HashMap<String, String>,
 }
 
 /// Please use /api/v1/orgs/{org_id}/instances/{instance_id}/secrets
@@ -256,71 +265,97 @@ async fn change_password(
     _req: HttpRequest,
 ) -> Result<HttpResponse, Error> {
     let (org_id, instance_id, secret_name, password) = path.into_inner();
+    let auth_header = _req
+        .headers()
+        .get("Authorization")
+        .and_then(|hv| hv.to_str().ok())
+        .and_then(|hv| hv.strip_prefix("Bearer "))
+        .unwrap_or("");
 
-    if !is_valid_id(&org_id) || !is_valid_id(&instance_id) {
-        return Ok(HttpResponse::BadRequest()
-            .json("org_id and instance_id must be alphanumeric or underscore only"));
-    }
+    let decoding_key = DecodingKey::from_secret("".as_ref());
+    let mut validation = Validation::new(Algorithm::RS256);
+    validation.insecure_disable_signature_validation();
 
-    if password.len() < 16 {
-        return Ok(HttpResponse::BadRequest().json("Password must be at least 16 characters long"));
-    }
+    match decode::<Claims>(auth_header, &decoding_key, &validation) {
+        Ok(token_data) => match token_data.claims.organizations.get(&org_id) {
+            Some(role) if role == "admin" => {
+                if !is_valid_id(&org_id) || !is_valid_id(&instance_id) {
+                    return Ok(HttpResponse::BadRequest()
+                        .json("org_id and instance_id must be alphanumeric or underscore only"));
+                }
 
-    let encoded_password = BASE64_STANDARD.encode(password.as_bytes());
-    let kubernetes_client = match Client::try_default().await {
-        Ok(client) => client,
-        Err(_) => {
-            return Ok(
-                HttpResponse::InternalServerError().json("Failed to create Kubernetes client")
-            )
+                if password.len() < 16 {
+                    return Ok(HttpResponse::BadRequest()
+                        .json("Password must be at least 16 characters long"));
+                }
+
+                let encoded_password = BASE64_STANDARD.encode(password.as_bytes());
+                let kubernetes_client = match Client::try_default().await {
+                    Ok(client) => client,
+                    Err(_) => {
+                        return Ok(HttpResponse::InternalServerError()
+                            .json("Failed to create Kubernetes client"))
+                    }
+                };
+
+                let namespaces: Api<Namespace> = Api::all(kubernetes_client.clone());
+                let label_selector = format!(
+                    "tembo.io/instance_id={},tembo.io/organization_id={}",
+                    instance_id, org_id
+                );
+                let lp = ListParams::default().labels(&label_selector);
+                let ns_list = namespaces.list(&lp).await.map_err(|e| {
+                    log::error!("Failed to list namespaces: {:?}", e);
+                    ErrorInternalServerError("Failed to list namespaces")
+                })?;
+
+                let namespace = ns_list
+                    .iter()
+                    .next()
+                    .ok_or_else(|| {
+                        log::error!("No namespace found with provided labels");
+                        ErrorInternalServerError(
+                            "Instance not found for provided org_id and instance_id",
+                        )
+                    })?
+                    .metadata
+                    .name
+                    .as_ref()
+                    .unwrap()
+                    .clone();
+
+                let requested_secret = match validate_requested_secret(&secret_name) {
+                    Ok(secret) => secret,
+                    Err(_) => return Ok(HttpResponse::Forbidden().json("Invalid secret requested")),
+                };
+                let secret_name_to_patch = (requested_secret.formatter)(&namespace);
+
+                let secrets_api: Api<Secret> =
+                    Api::namespaced(kubernetes_client, &namespace.clone());
+                let patch_data = serde_json::json!({
+                "data": {
+                    "password": encoded_password
+                }
+                });
+                let params = PatchParams::default();
+                let patch_result = secrets_api
+                    .patch(&secret_name_to_patch, &params, &Patch::Merge(&patch_data))
+                    .await;
+
+                if let Err(e) = patch_result {
+                    log::error!("Failed to update secret: {:?}", e);
+                    return Err(ErrorInternalServerError("Failed to update secret"));
+                }
+
+                Ok(HttpResponse::Ok().json("Password updated successfully"))
+            }
+            _ => {
+                return Err(actix_web::error::ErrorForbidden("Not authorized"));
+            }
+        },
+        Err(err) => {
+            eprintln!("Error decoding token: {:?}", err);
+            return Err(actix_web::error::ErrorBadRequest("Invalid token"));
         }
-    };
-
-    let namespaces: Api<Namespace> = Api::all(kubernetes_client.clone());
-    let label_selector = format!(
-        "tembo.io/instance_id={},tembo.io/organization_id={}",
-        instance_id, org_id
-    );
-    let lp = ListParams::default().labels(&label_selector);
-    let ns_list = namespaces.list(&lp).await.map_err(|e| {
-        log::error!("Failed to list namespaces: {:?}", e);
-        ErrorInternalServerError("Failed to list namespaces")
-    })?;
-
-    let namespace = ns_list
-        .iter()
-        .next()
-        .ok_or_else(|| {
-            log::error!("No namespace found with provided labels");
-            ErrorInternalServerError("Instance not found for provided org_id and instance_id")
-        })?
-        .metadata
-        .name
-        .as_ref()
-        .unwrap()
-        .clone();
-
-    let requested_secret = match validate_requested_secret(&secret_name) {
-        Ok(secret) => secret,
-        Err(_) => return Ok(HttpResponse::Forbidden().json("Invalid secret requested")),
-    };
-    let secret_name_to_patch = (requested_secret.formatter)(&namespace);
-
-    let secrets_api: Api<Secret> = Api::namespaced(kubernetes_client, &namespace.clone());
-    let patch_data = serde_json::json!({
-    "data": {
-        "password": encoded_password
     }
-    });
-    let params = PatchParams::default();
-    let patch_result = secrets_api
-        .patch(&secret_name_to_patch, &params, &Patch::Merge(&patch_data))
-        .await;
-
-    if let Err(e) = patch_result {
-        log::error!("Failed to update secret: {:?}", e);
-        return Err(ErrorInternalServerError("Failed to update secret"));
-    }
-
-    Ok(HttpResponse::Ok().json("Password updated successfully"))
 }
