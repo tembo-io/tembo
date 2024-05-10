d7ad57c5e4747a1904efef1498b4e3f6e1001215
Nick Hudson <nick.hudson@gmail.com>
2024-05-10 11:04:06 -0500

    rework how we lookup the last WAL archive time

--- tembo-operator/src/cloudnativepg/archive/wal.rs
+++ tembo-operator/src/cloudnativepg/archive/wal.rs
@@ -1,44 +1,63 @@
-use crate::{
-    apis::coredb_types::CoreDB,
-    cloudnativepg::{clusters::ClusterStatusConditionsStatus, cnpg::get_cluster},
-    Context,
-};
+use crate::{apis::coredb_types::CoreDB, Context};
 use chrono::{DateTime, Utc};
-use kube::{runtime::controller::Action, ResourceExt};
+use kube::runtime::controller::Action;
 use std::sync::Arc;
-use tracing::error;
 
-// Find status of the last time a WAL archive was successful and retrun the date
+const WAL_ARCHIVE_STATUS_QUERY: &str = r#"
+SELECT
+    to_char((last_archived_time::timestamp), 'YYYY-MM-DD"T"HH24:MI:SS.US"Z"')
+FROM pg_stat_archiver;
+"#;
+
+// WalArchiveStatus struct to hold the status of the WAL archive
+// for now we are only interested in the last archived WAL time
+#[derive(Debug, Clone)]
+pub struct WalArchiveStatus {
+    pub last_archived_time: Option<DateTime<Utc>>,
+}
+
+/// get_wal_archive_status queries the pg_stat_archiver table to get the status of the WAL archive
+/// Returns a WalArchiveStatus struct
+pub async fn get_wal_archive_status(
+    cdb: &CoreDB,
+    ctx: Arc<Context>,
+) -> Result<WalArchiveStatus, Action> {
+    let query = cdb
+        .psql(
+            WAL_ARCHIVE_STATUS_QUERY.to_string(),
+            "postgres".to_string(),
+            ctx.clone(),
+        )
+        .await?;
+
+    let last_archived_time = query.get_field(0).and_then(|s| parse_date_time(&s));
+
+    // Convert the query result into a WalArchiveStatus struct
+    let wal_archive_status = WalArchiveStatus { last_archived_time };
+
+    Ok(wal_archive_status)
+}
+
+/// Parses a date-time string in ISO 8601 format and converts it to a `DateTime<Utc>`.
+fn parse_date_time(date_str: &str) -> Option<DateTime<Utc>> {
+    DateTime::parse_from_rfc3339(date_str)
+        .map(|dt| dt.with_timezone(&Utc))
+        .ok()
+}
+
+// Find status of the last time a WAL archive was successful and return the date
 pub async fn reconcile_last_archive_status(
     cdb: &CoreDB,
     ctx: Arc<Context>,
 ) -> Result<Option<DateTime<Utc>>, Action> {
-    let name = cdb.name_any();
+    // Get the WAL archive status from the get_wal_archive_status function
+    let wal_archive_status = get_wal_archive_status(cdb, ctx.clone()).await?;
 
-    let cluster = get_cluster(cdb, ctx.clone()).await;
-    match cluster {
-        Some(cluster) => {
-            if let Some(status) = &cluster.status {
-                if let Some(conditions) = &status.conditions {
-                    for condition in conditions {
-                        if condition.r#type == "ContinuousArchiving"
-                            && condition.status == ClusterStatusConditionsStatus::True
-                        {
-                            let last_transition_time = &condition.last_transition_time;
-                            if let Ok(last_transition_time) =
-                                DateTime::parse_from_rfc3339(last_transition_time)
-                            {
-                                return Ok(Some(last_transition_time.with_timezone(&Utc)));
-                            }
-                        }
-                    }
-                }
-            }
-            Ok(None)
-        }
-        None => {
-            error!("Failed to get cluster: {}", &name);
-            Err(Action::requeue(tokio::time::Duration::from_secs(300)))
-        }
+    // If the last archived WAL time is None, return None
+    if wal_archive_status.last_archived_time.is_none() {
+        return Ok(None);
     }
+
+    // If wal_archive_status.last_archived_time is Some, return the last archived time
+    Ok(wal_archive_status.last_archived_time)
 }
