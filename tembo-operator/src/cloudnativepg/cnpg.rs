d9ec67a2ff562196bcb429803b2db447bfa17935
Steven Miller <sjmiller609@gmail.com>
2024-06-06 10:11:24 -0400

    Only fence restore pods if they need a trunk install

--- tembo-operator/src/cloudnativepg/cnpg.rs
+++ tembo-operator/src/cloudnativepg/cnpg.rs
@@ -1,5 +1,6 @@
 use crate::apis::coredb_types;
 use crate::apis::coredb_types::Restore;
+use crate::extensions::install::find_trunk_installs_to_pod;
 use crate::ingress_route_crd::{
     IngressRoute, IngressRouteRoutes, IngressRouteRoutesKind, IngressRouteRoutesServices,
     IngressRouteRoutesServicesKind, IngressRouteSpec, IngressRouteTls,
@@ -887,7 +888,10 @@ async fn pods_to_fence(cdb: &CoreDB, ctx: Arc<Context>) -> Result<Vec<String>, A
         let mut pod_names_to_fence = Vec::new();
         for i in 1..=cdb.spec.replicas {
             let pod_name = format!("{}-{}", &cdb.name_any(), i);
-            pod_names_to_fence.push(pod_name);
+            let trunk_installs = find_trunk_installs_to_pod(cdb, &pod_name);
+            if !trunk_installs.is_empty() {
+                pod_names_to_fence.push(pod_name);
+            }
         }
 
         debug!(
