f4abc3f6ff46142b9d1825b21aec7e95889ba18a
Adam Hendel <ChuckHend@users.noreply.github.com>
2024-06-29 14:39:29 -0500

    Update 20240626175841_fdw.sql

--- inference-gateway/migrations/20240626175841_fdw.sql
+++ inference-gateway/migrations/20240626175841_fdw.sql
@@ -1,19 +1,19 @@
 CREATE EXTENSION IF NOT EXISTS postgres_fdw;
 CREATE EXTENSION IF NOT EXISTS pg_cron;
 
-CREATE SERVER IF NOT EXISTS foreign_validation_server
+CREATE SERVER IF NOT EXISTS cp_queue_server
 FOREIGN DATA WRAPPER postgres_fdw
 OPTIONS (host 'localhost', port '5432', dbname 'postgres');
 
 CREATE USER MAPPING IF NOT EXISTS FOR postgres
-SERVER foreign_validation_server
+SERVER cp_queue_server
 OPTIONS (user 'postgres', password 'postgres');
 
 CREATE FOREIGN TABLE IF NOT EXISTS fdw_paid_organizations (
     organization_id text NOT NULL,
     has_cc boolean not null default false
 )
-SERVER foreign_validation_server
+SERVER cp_queue_server
 OPTIONS (schema_name 'public', table_name 'paid_organizations');
 
 SELECT cron.schedule('refresh-validations', '1 second', $$
