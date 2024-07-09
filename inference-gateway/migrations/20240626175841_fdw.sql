76d3cc8e27085dddb440a9d5260fa8d4288c7abc
Adam Hendel <hendel.adam@gmail.com>
2024-07-09 16:15:21 -0500

    drop replace

--- inference-gateway/migrations/20240626175841_fdw.sql
+++ inference-gateway/migrations/20240626175841_fdw.sql
@@ -11,19 +11,20 @@ OPTIONS (user 'postgres', password 'postgres');
 
 CREATE FOREIGN TABLE IF NOT EXISTS fdw_paid_organizations (
     organization_id text NOT NULL,
-    has_cc boolean not null default false
+    has_cc boolean not null default false,
+    last_updated_at timestamp with time zone not null default now()
 )
 SERVER cp_queue_server
 OPTIONS (schema_name 'billing', table_name 'paid_organizations');
 
-SELECT cron.schedule('refresh-validations', '1 second', $$
+SELECT cron.schedule('refresh-validations', '* * * * *', $$
+    BEGIN;
+    TRUNCATE inference.org_validation;
     INSERT INTO inference.org_validation
     SELECT 
         organization_id as org_id,
         has_cc as valid,
         now() as last_updated_at
-    FROM fdw_paid_organizations
-    ON CONFLICT (org_id, valid) DO UPDATE SET
-        valid = EXCLUDED.valid,
-        last_updated_at = EXCLUDED.last_updated_at
+    FROM fdw_paid_organizations;
+    COMMIT;
 $$);
