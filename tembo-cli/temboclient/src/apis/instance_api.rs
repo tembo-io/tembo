f75c1bb481ae4631be6db4958054c29f041ba664
Joshua Jerin <joshuaxjerin@gmail.com>
2024-10-03 23:03:10 +0530

    create_instance

--- tembo-cli/temboclient/src/apis/instance_api.rs
+++ tembo-cli/temboclient/src/apis/instance_api.rs
@@ -112,26 +112,27 @@ pub async fn create_instance(
     configuration: &configuration::Configuration,
     org_id: &str,
     create_instance: crate::models::CreateInstance,
-) -> Result<crate::models::Instance, Error<CreateInstanceError>> {
+) -> Result<crate::models::Instance, Error<()>> {
     let local_var_configuration = configuration;
 
     let local_var_client = &local_var_configuration.client;
 
     let local_var_uri_str = format!(
-        "{}/api/v1/orgs/{org_id}/instances",
+        "{}/api/v1/orgs/{}/instances",
         local_var_configuration.base_path,
-        org_id = crate::apis::urlencode(org_id)
+        crate::apis::urlencode(org_id)
     );
     let mut local_var_req_builder =
-        local_var_client.request(reqwest::Method::POST, local_var_uri_str.as_str());
+        local_var_client.request(reqwest::Method::POST, &local_var_uri_str);
 
     if let Some(ref local_var_user_agent) = local_var_configuration.user_agent {
-        local_var_req_builder =
-            local_var_req_builder.header(reqwest::header::USER_AGENT, local_var_user_agent.clone());
+        local_var_req_builder = local_var_req_builder
+            .header(reqwest::header::USER_AGENT, local_var_user_agent.clone());
     }
     if let Some(ref local_var_token) = local_var_configuration.bearer_access_token {
-        local_var_req_builder = local_var_req_builder.bearer_auth(local_var_token.to_owned());
-    };
+        local_var_req_builder =
+            local_var_req_builder.bearer_auth(local_var_token.to_owned());
+    }
     local_var_req_builder = local_var_req_builder.json(&create_instance);
 
     let local_var_req = local_var_req_builder.build()?;
@@ -140,19 +141,18 @@ pub async fn create_instance(
     let local_var_status = local_var_resp.status();
     let local_var_content = local_var_resp.text().await?;
 
-    if !local_var_status.is_client_error() && !local_var_status.is_server_error() {
+    if local_var_status.is_success() {
         serde_json::from_str(&local_var_content).map_err(Error::from)
     } else {
-        let local_var_entity: Option<CreateInstanceError> =
-            serde_json::from_str(&local_var_content).ok();
         let local_var_error = ResponseContent {
             status: local_var_status,
             content: local_var_content,
-            entity: local_var_entity,
+            entity: None,
         };
         Err(Error::ResponseError(local_var_error))
     }
 }
+
 
 /// Delete an existing Tembo instance
 pub async fn delete_instance(
