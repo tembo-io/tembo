357654dc3f73c2680d2955660042e0785f779930
Ian Stanton <ian@tembo.io>
2024-10-04 20:19:40 -0400

    Add UAMI create example
    Signed-off-by: Ian Stanton <ian@tembo.io>

--- conductor/src/azure/uami_builder.rs
+++ conductor/src/azure/uami_builder.rs
@@ -1,61 +1,28 @@
-use azure_identity::WorkloadIdentityCredential;
-use azure_core::auth::TokenCredential;
-use reqwest::Client;
-use reqwest::Url;
+use azure_identity::AzureCliCredential;
+use azure_mgmt_msi::models::{Identity, TrackedResource};
 use std::sync::Arc;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let tenant_id = "your_tenant_id".to_string();
-    let client_id = "your_client_id".to_string();
-    let subscription_id = "your_subscription_id";
-    let resource_group_name = "your_resource_group_name";
+pub async fn create_uami() -> Result<(), Box<dyn std::error::Error>> {
+    let credential = Arc::new(AzureCliCredential::new());
+    let subscription_id = AzureCliCredential::get_subscription().await?;
+    let resource_group_name = "ian".to_string();
+    let uami_name = "test-uami".to_string();
+    let client = azure_mgmt_msi::Client::builder(credential).build()?;
 
-    let http_client = Arc::new(reqwest::Client::new());
-    let authority_host = Url::parse("https://login.microsoftonline.com/")?;
-
-    let scopes = vec!["https://management.azure.com/.default"];
-    
-    let credential = WorkloadIdentityCredential::new(http_client, authority_host, tenant_id, client_id, None);
-    let token = credential.get_token(&scopes).await?;
-
-
-    let client = Client::new();
-    let base_url = format!("https://management.azure.com/subscriptions/{}/resourceGroups/{}/providers/Microsoft.ManagedIdentity/userAssignedIdentities", subscription_id, resource_group_name);
-
-    // Create a new User Assigned Managed Identity
-    let identity_name = "my-new-identity";
-    let url = format!("{}/{}", base_url, identity_name);
-    let body = json!({
-        "location": "eastus",
-    });
-    let headers = {
-        let mut headers = reqwest::header::HeaderMap::new();
-        headers.insert("Authorization", format!("Bearer {}", token).parse().unwrap());
-        headers.insert("Content-Type", "application/json".parse().unwrap());
-        headers
+    let uami_params = Identity {
+        tracked_resource: TrackedResource {
+            resource: Default::default(),
+            tags: None,
+            location: "eastus".to_string(),
+        },
+        properties: None,
     };
 
-    client.put(url)
-        .headers(headers)
-        .json(&body)
-        .send()
-        .await?
-        .text()
+    let uami_created = client
+        .user_assigned_identities_client()
+        .create_or_update(subscription_id, resource_group_name, uami_name, uami_params)
         .await?;
-
-    println!("Created User Assigned Managed Identity: {}", identity_name);
-
-    // Delete the User Assigned Managed Identity
-    let url = format!("{}/{}", base_url, identity_name);
-    client.delete(url)
-        .headers(headers)
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    println!("Deleted User Assigned Managed Identity: {}", identity_name);
-
+    println!("UAMI created: {uami_created:#?}");
     Ok(())
 }
