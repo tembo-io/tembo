58c5540f3cc2756044851aab2a6de7e56ff668cb
Ian Stanton <ian@tembo.io>
2024-10-07 12:54:17 -0400

    Use workload identity credentials
    Signed-off-by: Ian Stanton <ian@tembo.io>

--- conductor/src/azure/uami_builder.rs
+++ conductor/src/azure/uami_builder.rs
@@ -1,3 +1,4 @@
+use azure_core::auth::TokenCredential;
 use azure_identity::WorkloadIdentityCredential;
 use azure_identity::{AzureCliCredential, TokenCredentialOptions};
 use azure_mgmt_authorization;
@@ -6,23 +7,22 @@ use azure_mgmt_msi::models::{Identity, TrackedResource};
 use std::sync::Arc;
 
 // Get credentials from workload identity
-pub async fn get_credentials() -> Result<WorkloadIdentityCredential, Box<dyn std::error::Error>> {
+pub async fn get_credentials() -> Result<Arc<dyn TokenCredential>, Box<dyn std::error::Error>> {
     let options: TokenCredentialOptions = Default::default();
     let credential = WorkloadIdentityCredential::create(options)?;
-    Ok(credential)
+    Ok(Arc::new(credential))
 }
 
 // Create User Assigned Managed Identity
 pub async fn create_uami(
     resource_group: String,
+    subscription_id: String,
     instance_name: String,
     region: String,
+    credentials: Arc<dyn TokenCredential>,
 ) -> Result<Identity, Box<dyn std::error::Error>> {
-    let credential = Arc::new(AzureCliCredential::new());
-    let subscription_id = AzureCliCredential::get_subscription().await?;
-    let resource_group_name = resource_group;
     let uami_name = instance_name;
-    let msi_client = azure_mgmt_msi::Client::builder(credential.clone()).build()?;
+    let msi_client = azure_mgmt_msi::Client::builder(credentials).build()?;
 
     // Set parameters for User Assigned Managed Identity
     let uami_params = Identity {
@@ -39,7 +39,7 @@ pub async fn create_uami(
         .user_assigned_identities_client()
         .create_or_update(
             subscription_id.clone(),
-            resource_group_name,
+            resource_group,
             uami_name,
             uami_params,
         )
@@ -51,9 +51,11 @@ pub async fn create_uami(
 pub async fn create_role_assignment(
     subscription_id: &str,
     uami_id: String,
+    credentials: Arc<dyn TokenCredential>,
 ) -> Result<RoleAssignment, Box<dyn std::error::Error>> {
-    let credential = AzureCliCredential::new();
     let role_assignment_name = "00000000-0000-0000-0000-000000000000".to_string();
+    let role_assignment_client = azure_mgmt_authorization::Client::builder(credentials).build()?;
+    let scope = format!("/subscriptions/{subscription_id}");
 
     // Set parameters for Role Assignment
     let role_assignment_params = azure_mgmt_authorization::models::RoleAssignmentCreateParameters {
@@ -72,9 +74,6 @@ pub async fn create_role_assignment(
             delegated_managed_identity_resource_id: None,
         },
     };
-
-    let role_assignment_client = azure_mgmt_authorization::Client::builder(credential).build()?;
-    let scope = format!("/subscriptions/{subscription_id}");
 
     // Create Role Assignment
     let role_assignment_created = role_assignment_client
