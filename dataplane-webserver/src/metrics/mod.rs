11539fb11cfcfafbb0d6dac69a2aacabd4f80cbb
Nick Hudson <nick.hudson@gmail.com>
2025-03-09 20:10:20 -0500

    update dataplane-webserver

--- dataplane-webserver/src/metrics/mod.rs
+++ dataplane-webserver/src/metrics/mod.rs
@@ -1,6 +1,5 @@
 use crate::config::Config;
 use crate::metrics::types::{InstantQuery, RangeQuery};
-use actix_web::http::StatusCode;
 use actix_web::web::{Data, Query};
 use actix_web::HttpResponse;
 use log::error;
@@ -20,15 +19,15 @@ async fn prometheus_response(response: Response) -> HttpResponse {
         }
     };
 
-    match status_code {
-        StatusCode::OK => HttpResponse::Ok().json(json_response),
-        StatusCode::BAD_REQUEST => {
+    match status_code.as_u16() {
+        200 => HttpResponse::Ok().json(json_response),
+        400 => {
             HttpResponse::BadRequest().json("Prometheus reported the query is malformed")
         }
-        StatusCode::GATEWAY_TIMEOUT | StatusCode::SERVICE_UNAVAILABLE => {
+        504 | 503 => {
             HttpResponse::GatewayTimeout().json("Prometheus timeout")
         }
-        StatusCode::UNPROCESSABLE_ENTITY => {
+        422 => {
             if json_response["error"]
                 .to_string()
                 .contains("context deadline exceeded")
@@ -41,7 +40,7 @@ async fn prometheus_response(response: Response) -> HttpResponse {
         _ => {
             error!("{:?}: {:?}", status_code, &json_response);
             HttpResponse::InternalServerError()
-                .json("Prometheus returned an unexpected status code")
+                .json(format!("Unexpected response from Prometheus: {}", status_code))
         }
     }
 }
