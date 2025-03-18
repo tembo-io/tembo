45524af66557f96a4544427d02c381b9ddee374c
David E. Wheeler <david.wheeler@tembo.io>
2025-03-18 13:42:31 -0400

    Do not quote wildcard (#1191)

--- tembo-pod-init/src/init-dirs.sh
+++ tembo-pod-init/src/init-dirs.sh
@@ -8,6 +8,6 @@ dst=/var/lib/postgresql/init
 if [ -x /tmp/sync-volume.sh ]; then
     /tmp/sync-volume.sh "$dst"
 else
-    cp -p --recursive --update "$src/*" "$dst/"
+    cp -p --recursive --update "$src/"* "$dst/"
     rm --recursive --force "$dst/lost+found"
 fi
